46f7f6cd5ebe7f4396e3a929278349ce40fc76a8
Eduardo Bacarin <me@bacarin.dev>
2026-05-04 18:04:46 -0300

    Storage interface

---
+++ internal/storage/storage.go
@@ -0,0 +1,11 @@
+package storage
+
+import "io"
+
+type Engine interface {
+	Save(id string, data io.Reader) (string, error)
+
+	Get(path string) (io.ReadCloser, error)
+
+	Delete(path string) error
+}
